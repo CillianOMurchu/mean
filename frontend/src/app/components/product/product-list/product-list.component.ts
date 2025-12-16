1edadcf234c68bf42b5a7a34ee1a790340ad04ce
Cillian <cillian.murchu@gmail.com>
2025-12-16 02:28:41 +0100

    before adding scripts

--- frontend/src/app/components/product/product-list/product-list.component.ts
+++ frontend/src/app/components/product/product-list/product-list.component.ts
@@ -1,13 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { ApiService } from '@services/api.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ MatGridListModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- frontend/src/app/types/product.type.ts
+++ frontend/src/app/types/product.type.ts
@@ -1,6 +1,18 @@
+export interface ProductImage {
+  src: string;
+  alt: string;
+  position: number;
+  productUrl: string;
+}
+
 export interface ProductType {
   _id: string;
   name: string;
-  price: number;
   description: string;
+  category: string;
+  price: number;
+  stockQuantity: number;
+  images: ProductImage[];
+  createdAt: string;
+  updatedAt: string;
 }
