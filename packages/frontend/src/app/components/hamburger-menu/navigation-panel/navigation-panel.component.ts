193b1157f39bd0af54edf69eb4c9ccaa40dcb329
Cillian <cillian.murchu@gmail.com>
2025-12-19 23:23:12 +0100

    fix start

--- packages/frontend/src/app/components/hamburger-menu/navigation-panel/navigation-panel.component.ts
+++ packages/frontend/src/app/components/hamburger-menu/navigation-panel/navigation-panel.component.ts
@@ -1,18 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-import { LinkButtonComponent } from '@components/link-button/link-button.component';
-import { TreeMenuComponent } from '@components/tree-menu/tree-menu.component';
 import { routingConfig } from '@constants/routing-config.constant';
 
 @Component({
   selector: 'app-navigation-panel',
-  imports: [
-    MatListModule,
-    RouterModule,
-    TreeMenuComponent,
-    LinkButtonComponent,
-  ],
+  imports: [MatListModule, RouterModule],
   templateUrl: './navigation-panel.component.html',
   styleUrl: './navigation-panel.component.scss',
 })
