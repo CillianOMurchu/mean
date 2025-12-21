1cf25f12f73c6d876a063e8edb335e92e400b1d8
Cillian <cillian.murchu@gmail.com>
2025-12-21 15:31:44 +0100

    before tidy

--- packages/frontend/src/app/components/product/product-card/product-card.component.ts
+++ packages/frontend/src/app/components/product/product-card/product-card.component.ts
@@ -1,35 +1,24 @@
-import { CommonModule, CurrencyPipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ProductType } from 'src/app/types/product.interface';
 
 @Component({
   selector: 'app-product-card',
-  imports: [CurrencyPipe, MatButtonModule, MatIconModule, CommonModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
   @Input() product: ProductType | null = null;
 
-  /** quantity state (mobile-first) */
-  qty = signal(1);
-
-  @Output() addToCart = new EventEmitter<{
-    product: ProductType;
-    qty: number;
-  }>();
-
-  increment() {
-    this.qty.update((n) => Math.min(n + 1, 99));
-  }
-  decrement() {
-    this.qty.update((n) => Math.max(n - 1, 1));
-  }
-
-  onAdd() {
-    if (!this.product) return;
-    this.addToCart.emit({ product: this.product, qty: this.qty() });
+  get safeImages() {
+    const images =
+      Array.isArray(this.product?.images) && this.product?.images.length
+        ? this.product.images
+        : null;
+    console.log('safeImages called, returning:', images);
+    return images;
   }
 }
